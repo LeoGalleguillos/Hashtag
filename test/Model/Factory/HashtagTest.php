047c26910752341e98f9562bd4c99c9aeb698027
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-04-12 14:33:40 -0700

    Write preliminary test for hashtag factory

---
+++ test/Model/Factory/HashtagTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace LeoGalleguillos\HashtagTest\Model\Factory;
+
+use Exception;
+use LeoGalleguillos\Hashtag\Model\Factory as HashtagFactory;
+use LeoGalleguillos\Hashtag\Model\Table as HashtagTable;
+use PHPUnit\Framework\TestCase;
+
+class HashtagTest extends TestCase
+{
+    protected function setUp()
+    {
+        $this->hashtagTableMock   = $this->createMock(HashtagTable\Hashtag::class);
+        $this->hashtagFactory = new HashtagFactory\Hashtag(
+            $this->hashtagTableMock
+        );
+    }
+
+    public function testInitialize()
+    {
+        $this->assertInstanceOf(
+            HashtagFactory\Hashtag::class,
+            $this->hashtagFactory
+        );
+    }
+}
